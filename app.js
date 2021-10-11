537fb1469ea655519107bbfaa8c55382577f1023
K Sheenan <kaynesheenan+git@gmail.com>
2021-10-11 11:24:45 +0900

    catch error to display default image if request rejected

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cute-cats")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=korea")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         // document.p.style = ``
         document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1588043213440-fd9c881853e9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzM5MTg5MTk&ixlib=rb-1.2.1&q=80&w=1080)`
     })
