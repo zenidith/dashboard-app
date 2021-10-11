c43a93d4897d1cc213ac42f70e3bc53bd3494442
K Sheenan <kaynesheenan+git@gmail.com>
2021-10-11 10:24:59 +0900

    added author

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cute-cats")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        // document.p.style = ``
+        document.getElementById("author").textContent = `By: ${data.user.name}`
     })
