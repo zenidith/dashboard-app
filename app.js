c314dde7114e5bcfeece546df83d4aa78c7ca255
K Sheenan <kaynesheenan+git@gmail.com>
2021-10-14 12:03:55 +0900

    testing coingecko api

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .catch(err => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1588043213440-fd9c881853e9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzM5MTg5MTk&ixlib=rb-1.2.1&q=80&w=1080)`
     })
+
+    fetch("https://api.coingecko.com/api/v3/coins/solana")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
