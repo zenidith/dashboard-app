4ab0c0cbe69248c3de0b322517c71e0b9910a17e
K Sheenan <kaynesheenan+git@gmail.com>
2021-10-17 12:50:37 +0900

    geolocation weather call

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// background image and default
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=coffee")
     .then(res => res.json())
     .then(data => {
@@ -9,6 +10,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1588043213440-fd9c881853e9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzM5MTg5MTk&ixlib=rb-1.2.1&q=80&w=1080)`
     })
 
+// crypto tickers
     fetch("https://api.coingecko.com/api/v3/coins/solana")
     .then(res => {
         if (!res.ok) {
@@ -22,18 +24,34 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
             <span>${data.name}</span>
         `
         document.getElementById("crypto").innerHTML += `
-            <p>now: A$${data.market_data.current_price.aud}</p>
-            <p>high: A$${data.market_data.high_24h.aud}</p>
-            <p>low: A$${data.market_data.low_24h.aud}</p>
+            <p>&#127919: A$${data.market_data.current_price.aud}</p>
+            <p>&#128200: A$${data.market_data.high_24h.aud}</p>
+            <p>&#128201: A$${data.market_data.low_24h.aud}</p>
         `
     })
 
         
     .catch(err => console.error(err))
 
+
+// time
 function getCurrentTime() {
     const date = new Date()
     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
 }
 
 setInterval(getCurrentTime, 1000)
+
+// geolocation / weather call
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+});
