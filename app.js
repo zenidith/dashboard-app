71d054941e4440313149f86c38e6d99e81b8c903
K Sheenan <kaynesheenan+git@gmail.com>
2021-10-17 14:38:45 +0900

    geolocation weather call styling

--- app.js
+++ app.js
@@ -52,6 +52,12 @@ navigator.geolocation.getCurrentPosition(position => {
             return res.json()
         })
         .then(data => {
-            console.log(data)
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />    
+                <p>${Math.round(data.main.temp)} c</p>
+                <p class="weather-city">${data.name}</p>
+            `
         })
+        .catch(err => console.log(err))
 });
